70936fd40a7ea1955568bcf1f10ef9c0ff4376e8
Simon Whitty <simon@whileloop.com>
2022-03-06 16:12:33 +1100

    Version

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
   	  	.executable(name: "flyingfox", targets: ["FlyingFoxCLI"])
     ],
     dependencies: [
-        .package(url: "https://github.com/swhitty/FlyingFox.git", .upToNextMajor(from: "0.3.1"))
+        .package(url: "https://github.com/swhitty/FlyingFox.git", .upToNextMajor(from: "0.4.0"))
     ],
     targets: [
         .executableTarget(
